e5592eabf799f7708032696eb8590d7921e60c3b
shallowdream2 <1160234413@qq.com>
2025-11-21 06:09:48 +0000

    Deploying to gh-pages from @ shallowdream2/shallowdream2.github.io@8a81d874bf595158ec18c308d7d39731b28509c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Chang_Peng_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
